63664d87f9ffa511411d262677b311ee8113b4be
samirfaritov <samirfaritov6@gmail.com>
2023-10-06 11:19:16 +0500

    js8

---
+++ main.js
@@ -0,0 +1,61 @@
+// // let arr = [
+// //     "Salom",
+// //     81,
+// //     true,
+// //     false,
+// //     "Xayr",
+// //     {},
+// //     {},
+// //     [], [],
+// //     NaN,
+// //     null,
+// //     undefined
+// // ]
+
+// // console.log(
+// //     arr[3]
+// // );
+
+// let names = ["Ahmatbek", "Jasur", "Samir", "Javokhir", "Ilhom"]
+// let numbers = [1, 22, 34, 3, 46, 8676, 34535, 33]
+// // xamesha poparyadka mukunad
+// let a = numbers.sort((a,b) => a - b)
+
+// // let b = numbers.sort()
+
+// console.log(a);
+
+// console.log(names.sort());
+// // sarahba kushi mukunad
+// names.unshift('Ahmed')
+// names.unshift('Mehmed')
+
+// // axirashba kushi mukunad
+// names.push("Davlatshox")
+// names.push("Alisa")
+
+// // sarasha kur mukunad
+// names.pop()
+
+// // Axirasha ud mukunad
+// names.shift()
+
+// // Xamun elementa ud mukunad i xoxlasag alish mukunad
+// names.splice(6, 1, "Qodir")
+
+// console.log(names);
+
+let index = prompt()
+
+let arr = ["Aleksey", "Kartoshka", "Margarita", "Morgenshtern", "Monica"]
+console.log(arr);
+
+if (index <= arr && index <= arr) {
+    arr.splice(0,1)
+    arr.splice(0,2)
+    arr.splice(0,3)
+    arr.splice(0,4)
+    arr.splice(0,5)
+} else {
+    false
+}
